9d92266bf62ac95ecbf3bfa28b615e738b3ad7f6
klaramari <klara.fleisch@gmail.com>
2023-08-26 09:37:58 +0200

    Implement switch statement for computer choice
    computers choice is generated and then converted into a string to depict rock, paper or scissors

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
-console.log("hey")
+function getComputerChoice () {
+    let number = getRandomInt(1,3);
+    switch (number){
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function playRound(playerSelection, ComputerSelection){
+    return;
+}

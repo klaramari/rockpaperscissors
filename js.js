b760d2a84f007b80e4536a0c71cbf1bba5f444fa
klaramari <klara.fleisch@gmail.com>
2023-08-27 21:59:03 +0200

    Add graphical ui
    changed output from console to UI

--- js.js
+++ js.js
@@ -1,3 +1,16 @@
+const rock = document.querySelector('.rock')
+rock.addEventListener("click", () => playRound("ROCK", getComputerChoice()));
+
+const scissors = document.querySelector('.scissors')
+scissors.addEventListener("click", () => playRound("SCISSORS", getComputerChoice()));
+
+const paper = document.querySelector('.paper')
+paper.addEventListener("click", () => playRound("PAPER", getComputerChoice()));
+
+let pwins = 0;
+let cwins = 0;
+let tiess = 0;
+
 function getComputerChoice () {
     let number = getRandomInt(1,3);
     switch (number){
@@ -17,77 +30,118 @@ function getRandomInt(min, max) {
 }
 
 function playRound(playerSelection, computerSelection){
+
     playerSelection = playerSelection.toUpperCase()
-
+    const current = document.querySelector(".currentgame");
+    const playerwins = document.querySelector(".pwins");
+    const computerwins = document.querySelector(".cwins");
+    const ties = document.querySelector(".ties");
+    makeResultsVisible();
+    current.innerText = (`Playing ${playerSelection} against ${computerSelection}`);
+    ties.innerText = tiess;
+    playerwins.innerText = pwins;
+    computerwins.innerText = cwins;
     if (playerSelection == computerSelection.toUpperCase()) {
-        return 2
+        tiess++;
+        ties.innerText = tiess;
+        return;
     }
-
-    switch (playerSelection){
-        case "ROCK":
-            if (computerSelection == "Scissors"){
-                return 1
-            }
-            else if (computerSelection =="Paper"){
-                return 0
-            }
-            else {
-                return "Error"
-            }
-        case "PAPER":
-            if (computerSelection == "Scissors"){
-                return 0
-            }
-            else if (computerSelection =="Rock"){
-                return 1
-            }
-            else {
-                return "Error"
-            }
-        case "SCISSORS":
-            if (computerSelection == "Rock"){
-                return 0
-            }
-            else if (computerSelection =="Paper"){
-                return 1
-            }
-            else {
-                return "Error"
-            }
+    else {
+        switch (playerSelection){
+            case "ROCK":
+                if (computerSelection == "Scissors"){
+                    pwins++;
+                    break;
+                }
+                else if (computerSelection == "Paper"){
+                    cwins++;
+                    break;
+                }
+                else {
+                    console.log("error1");
+                    break;
+                }
+            case "PAPER":
+                if (computerSelection == "Scissors"){
+                    cwins++;
+                    break;
+                }
+                else if (computerSelection =="Rock"){
+                    pwins++;
+                    break;
+                }
+                else {
+                    console.log("error2");
+                    break;
+                }
+            case "SCISSORS":
+                if (computerSelection == "Rock"){
+                    cwins++;
+                    break;
+                }
+                else if (computerSelection =="Paper"){
+                    pwins++;
+                    break;
+                }
+                else {
+                    console.log("error3");
+                    break;
+                }
+        }
+        playerwins.innerText = pwins;
+        computerwins.innerText = cwins;
+    }
+    const all = document.querySelector("#buttons")
+    if (cwins == 5){
+        all.innerText = "Computer won!"
+    }
+    else if (pwins == 5){
+        all.innerText = "Player won!"
     }
 }
 
+function makeResultsVisible(){
+    const results = document.querySelector('#results');
+    results.classList.remove('invisible')
+}
 
-function game(){
-    let playerSelection;
-    let computerSelection;
-    let counter = 0;
-    let round;
-    let ties = 0;
+// function game(){
+//     let playerSelection;
+//     let computerSelection;
+//     let counterc = 0;
+//     let counterp = 0;
+//     let round;
+//     let ties = 0;
 
-    for (let i = 0; i < 5; i++){
-        playerSelection = prompt(`Round ${i + 1}! Pick your weapon`).toUpperCase()
-        while (playerSelection != "ROCK" && playerSelection != "SCISSORS" && playerSelection != "PAPER"){
-            playerSelection = prompt("Not a valid selection. Retype.").toUpperCase()
-        }
-        computerSelection = getComputerChoice();
-        console.log(`Computers choice was ${computerSelection}.`)
-        round = playRound(playerSelection, computerSelection)
+//     while (counterc < 5 && counterp < 5){
+//         playerSelection = prompt(`Round ${i + 1}! Pick your weapon`).toUpperCase()
+//         while (playerSelection != "ROCK" && playerSelection != "SCISSORS" && playerSelection != "PAPER"){
+//             playerSelection = prompt("Not a valid selection. Retype.").toUpperCase()
+//         }
+//         computerSelection = getComputerChoice();
+//         console.log(`Computers choice was ${computerSelection}.`)
+//         round = playRound(playerSelection, computerSelection)
         
-        if (round == 1){
-            console.log("You won this one");
-            counter += 1;
-        }
-        else if (round == 0){
-            console.log("You lost this one");
-        }
-        else {
-            console.log("Tie!")
-            ties += 1;
-        }
-    }
+//         if (round == 1){
+//             console.log("You won this one");
+//             counter += 1;
+//         }
+//         else if (round == 0){
+//             console.log("You lost this one");
+//         }
+//         else {
+//             console.log("Tie!")
+//             ties += 1;
+//         }
+//     }
 
-    console.log(`You won ${counter} out of 5 games. There was ${ties} Tie(s). Congrats!`)
+//     console.log(`You won ${counter} out of 5 games. There was ${ties} Tie(s). Congrats!`)
+// }
+
+function clicked(){
+    console.log("clicked")
 }
+
+
 
 
